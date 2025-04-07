e2cfb7c5a8f057742cd06785af8feaa51f10bb8c
Ehiane <e.oigiagbe@wsu.edu>
2025-04-07 10:54:27 -0700

    Implements basic login functionality
    Adds a login form to the index page.
    The form submits username and password to the server and redirects
    to the courses page upon successful authentication.
    
    Renames the users route and file to courses, indicating the
    future intention of the application.
    
    Includes a logo image.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', function(req, res, next) {
+  console.log(req.body.username + " - " + req.body.password)
+  res.redirect("/courses"); // redirects to the courses page
+});
+
+
 module.exports = router;
