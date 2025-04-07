73405315341b210c4de3a74d55b8c1baa7ce8217
Ehiane <e.oigiagbe@wsu.edu>
2025-04-07 12:47:14 -0700

    replaced old db.js logic with sequelize ORM

--- db.js
+++ db.js
@@ -1,30 +1,8 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database/cmsdb.sqlite');
+const {Sequelize} = require('sequelize')
 
+const sequelize = Sequelize({
+    dialect: 'sqlite',
+    storage: './database/cmsdbsequelize.sqlite'
+})
 
-// open the database
-let sql = `SELECT * FROM USER WHERE username=? and password=?`;
-
-function getRow(sql, values) {
-    return new Promise((resolve, reject) => {
-        // username=? == subu, password=> == 1234
-        db.get(sql, values, (err, row) => {
-            if (err) {
-                reject(err);
-            }
-            else{
-                resolve(row)
-            }
-        });
-    })
-}
-
-async function getRowTest(){
-    // getRow(sql, ["subu", "1234"]).then((row) => console.log(row))
-    const row = await getRow(sql, ["subu", "1234"])
-    console.log(row)
-}
-
-getRowTest().then(() => console.log("Query Completed"))
-
-module.exports = {getRow}
+module.exports = sequelize
